f2b2d18fb6faec701b0937120d2af4ee1ecf5c91
Frail Bongat <frail.bongat@gmail.com>
2021-11-07 17:42:30 +0800

    🔍 Add meta description for better seo

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,11 @@ import Navbar from 'components/Navbar'
 const MyApp = ({ Component, pageProps }) => (
   <>
     <Head>
-      <title>Frail Bongat | Front End Developer</title>
+      <title>Frail Bongat | Home</title>
+      <meta
+        name='description'
+        content='Frail Bongat | A professional front end developer that focuses on javascript and react, based in the philippines'
+      />
     </Head>
 
     <Navbar />
