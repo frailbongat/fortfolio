89160f3f3af4b1609ed19a3d5ec911dff90f78ec
Frail Bongat <frail.bongat@gmail.com>
2021-11-07 11:18:30 +0800

    ⚗️ Add styled test component

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,13 +1,31 @@
 import { Text } from '@nextui-org/react'
+import styled from 'styled-components'
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`
 
 const Home = () => (
-  <Text h1>
-    Welcome to a whole new{' '}
-    <Text color='primary' span>
-      typescript
-    </Text>{' '}
-    project!
-  </Text>
+  <>
+    <Text h1>
+      Welcome to a whole new{' '}
+      <Text color='primary' span>
+        typescript
+      </Text>{' '}
+      project!
+    </Text>
+
+    <Wrapper>
+      <Title>I'm fucking styled 💅</Title>
+    </Wrapper>
+  </>
 )
 
 export default Home
