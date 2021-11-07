3825e2d9ffecd7aa2e5250fba594739146a891a7
Frail Bongat <frail.bongat@gmail.com>
2021-11-07 12:09:01 +0800

    ⚗️ Experiment for nextui document file

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,32 +1,65 @@
-import { CssBaseline } from '@nextui-org/react'
-import { ServerStyleSheet } from 'styled-components'
+// import { CssBaseline } from '@nextui-org/react'
+// import { ServerStyleSheet } from 'styled-components'
+// import Document, { Html, Head, Main, NextScript } from 'next/document'
+
+// class MyDocument extends Document {
+//   static async getInitialProps(ctx) {
+//     const styles = CssBaseline.flush()
+//     const sheet = new ServerStyleSheet()
+//     const originalRenderPage = ctx.renderPage
+
+//     try {
+//       ctx.renderPage = () =>
+//         originalRenderPage({
+//           enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
+//         })
+
+//       const initialProps = await Document.getInitialProps(ctx)
+//       return {
+//         ...initialProps,
+//         styles: (
+//           <>
+//             {initialProps.styles}
+//             {styles}
+//             {sheet.getStyleElement()}
+//           </>
+//         ),
+//       }
+//     } finally {
+//       sheet.seal()
+//     }
+//   }
+
+//   render() {
+//     return (
+//       <Html>
+//         <Head />
+//         <body>
+//           <Main />
+//           <NextScript />
+//         </body>
+//       </Html>
+//     )
+//   }
+// }
+
+// export default MyDocument
 import Document, { Html, Head, Main, NextScript } from 'next/document'
+import { CssBaseline } from '@nextui-org/react'
 
 class MyDocument extends Document {
   static async getInitialProps(ctx) {
+    const initialProps = await Document.getInitialProps(ctx)
     const styles = CssBaseline.flush()
-    const sheet = new ServerStyleSheet()
-    const originalRenderPage = ctx.renderPage
 
-    try {
-      ctx.renderPage = () =>
-        originalRenderPage({
-          enhanceApp: App => props => sheet.collectStyles(<App {...props} />),
-        })
-
-      const initialProps = await Document.getInitialProps(ctx)
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            {styles}
-            {sheet.getStyleElement()}
-          </>
-        ),
-      }
-    } finally {
-      sheet.seal()
+    return {
+      ...initialProps,
+      styles: (
+        <>
+          {initialProps.styles}
+          {styles}
+        </>
+      ),
     }
   }
 
