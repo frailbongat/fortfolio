d16ed46d8f009bfc57de7b248a8ae1ffafd005ab
Frail Bongat <frail.bongat@gmail.com>
2021-11-07 17:44:52 +0800

    💬 Update title text

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,7 +5,7 @@ import Navbar from 'components/Navbar'
 const MyApp = ({ Component, pageProps }) => (
   <>
     <Head>
-      <title>Frail Bongat | Home</title>
+      <title>Frail Bongat | Front End Developer</title>
       <meta
         name='description'
         content='Frail Bongat | A professional front end developer that focuses on javascript and react, based in the philippines'
