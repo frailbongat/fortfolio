6e70b7e194e149deb8be23ebe99fcb3ba7da238e
Frail Bongat <frail.bongat@gmail.com>
2021-11-09 13:58:51 +0800

    ⚗️ New build for pwa

--- next.config.js
+++ next.config.js
@@ -9,7 +9,7 @@ module.exports = withPWA({
     dest: 'public',
     register: true,
     skipWaiting: true,
-    publicExcludes: ['!robots.txt', '!sitemap.xml.gz'],
+    publicExcludes: ['!robots.txt'],
     disable: process.env.NODE_ENV === 'development',
   },
 })
