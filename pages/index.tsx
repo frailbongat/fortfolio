6490394a4c5eefa6eadafd104373bfd91382c4c1
Frail Bongat <frail.bongat@gmail.com>
2021-11-09 14:29:01 +0800

    ♿️ Add text bg color

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,6 @@
 const Home = () => (
   <>
-    <p className='text-primary font-body'>
+    <p className='text-primary font-body bg-gray-900'>
       Welcome to a whole new typescript project!
     </p>
   </>
