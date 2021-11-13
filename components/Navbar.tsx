ce8d7cc40834b21ee4e424aca34975aecc9ea79a
Frail Bongat <frail.bongat@gmail.com>
2021-11-13 20:43:04 +0800

    🎨 Add atomic design pattern

--- components/Navbar.tsx
+++
@@ -1,3 +0,0 @@
-const Navbar = () => <h1>Navbar goes here</h1>
-
-export default Navbar
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,4 @@
 import Head from 'next/head'
-import Navbar from 'components/Navbar'
 import 'styles/globals.css'
 
 const MyApp = ({ Component, pageProps }) => (
@@ -12,7 +11,6 @@ const MyApp = ({ Component, pageProps }) => (
       />
     </Head>
 
-    <Navbar />
     <Component {...pageProps} />
   </>
 )
