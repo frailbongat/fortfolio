601a541fc70e75b5d78012455145a3c78e70ff20
Frail Bongat <frail.bongat@gmail.com>
2021-11-14 11:41:01 +0800

    ⚗️ Test component to page

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,12 @@
+import Test from 'components/Test'
+
 const Home = () => (
   <>
     <p className='text-primary bg-gray-900'>
       Welcome to a whole new typescript project!
     </p>
+
+    <Test />
   </>
 )
 
